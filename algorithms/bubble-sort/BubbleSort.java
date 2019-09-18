4f6e400aaa7050295f84bfbc2474678f403e3ddb
Yassin AJDI <yassin.ajdi@gmail.com>
2019-09-18 23:58:09 +0100

    Add algorithm: bubble sort

---
+++ algorithms/bubble-sort/BubbleSort.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+// Implementation of Bubble Sort in Java
+public class BubbleSort {
+    public void sort(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            boolean swapped = false;
+            for (int j = 1; j < n - i; j++) {
+                if (array[j - 1] > array[j]) {
+                    int temp = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = temp;
+                    swapped = true;
+                }
+            }
+            System.out.println(Arrays.toString(array));
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+}
---
+++ algorithms/bubble-sort/test.kt
@@ -0,0 +1,7 @@
+import java.util.*
+
+fun main() {
+    val array = intArrayOf(2, 1, 4, 0)
+    BubbleSort().sort(array)
+    println(array.contentToString())
+}
